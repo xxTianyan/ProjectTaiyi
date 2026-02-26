12ced0dad1f4b5908632c98bd0c4101efe56ae54
Yisan_ <xuty1999@outlook.com>
2026-02-26 15:56:06 +0800

    rename

---
+++ solver/mujoco/OrderSolver.h
@@ -0,0 +1,18 @@
+//
+// Created by tianyan on 2/26/26.
+//
+
+#ifndef TAIYI_ORDERSOLVER_H
+#define TAIYI_ORDERSOLVER_H
+#include "ISolver.h"
+
+class OrderSolver : ISolver {
+
+
+
+
+
+};
+
+
+#endif //TAIYI_ORDERSOLVER_H
